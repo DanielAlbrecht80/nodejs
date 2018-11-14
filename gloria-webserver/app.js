1b5afc5415cc9e0345a0ee8bff14804de389bd25
DanielAlbrecht80 <44770639+DanielAlbrecht80@users.noreply.github.com>
2018-11-14 14:27:43 +0100

    mods

--- gloria-webserver/app.js
+++ gloria-webserver/app.js
@@ -2,8 +2,8 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 const fileUpload = require("express-fileupload");
+var logger = require("morgan");
 const cors = require("cors");
 
 var indexRouter = require("./routes/index");
